f2e4c53fa83e7bbfd8c5756d4c2b67237733a98e
hanumeee <saharany2413@gmail.com>
2019-04-02 00:01:56 +0700

    up

---
+++ database/migrations/2019_04_01_152938_topup_balance.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TopupBalance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topup_balance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('phone_number');
+            $table->string('value');
+            $table->integer('created_userid')->nullable();
+            $table->integer('updated_userid')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topup_balance');
+    }
+}
---
+++ database/migrations/2019_04_01_154021_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product');
+            $table->string('shipping_address');
+            $table->string('price');
+            $table->integer('created_userid')->nullable();
+            $table->integer('updated_userid')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');        
+    }
+}
